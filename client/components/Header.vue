4b000406ca27080953e46fa32bf24b919ae8cae2
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-10 13:20:41 +0100

    Add scss plugin and reset css

---
+++ client/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+const today = new Date();
+const date = today.getDate();
+const day = today.getDay();
+</script>
+
+<template>
+  <header class="main-header">
+    <p>{{ day }}</p>
+    <Button @click="onClick">test</Button>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.main-header {
+  background: var(--primary-700);
+}
+</style>
