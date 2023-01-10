6a8b9a3099adcb03de57b11d163829abf58c0a56
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-10 14:06:58 +0100

    Add primeflex

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config';
 
 import 'primevue/resources/themes/md-light-deeppurple/theme.css';
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 app.use(PrimeVue);
