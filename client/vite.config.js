c909e26ee7d432095164223bff7477e9df38731c
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-10 12:38:51 +0100

    Remove unneeded rollupOptions

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,5 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  plugins: [vue()]
 });
