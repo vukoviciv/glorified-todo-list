ec75b8ccd923c38706e844486fdf088d70358b0b
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-13 08:59:28 +0100

    add transition to task items

--- client/components/TaskItem.vue
+++ client/components/TaskItem.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex mb-3 p-3">
     <Checkbox
-      v-model="task.done"
+      v-model="isDone"
       @input="$emit('update:task', $event, task)"
       :input-id="task.id"
-      :input-props="{ 'aria-describedby': 'task-description' }"
+      :input-props="inputProps"
       :binary="true" />
     <div class="ml-3">
-      <label :for="task.id">{{ task.name }}</label>
-      <p id="task-description" class="mt-2">{{ task.description }}</p>
+      <label class="checkbox-label" :for="task.id">{{ task.name }}</label>
+      <p id="task-description" class="mt-2 pl-2">{{ task.description }}</p>
     </div>
   </div>
 </template>
@@ -18,8 +18,23 @@ import Checkbox from 'primevue/checkbox';
 import { ref } from 'vue';
 
 const props = defineProps({
-  item: { type: Object, required: true }
+  item: { type: Object, required: true },
+  done: { type: Boolean, required: true }
 });
 
 const task = ref(props.item);
+const isDone = ref(props.done);
+const inputProps = { 'aria-describedby': 'task-description' };
 </script>
+
+<style lang="scss" scoped>
+.checkbox-label {
+  padding: 0.5rem;
+
+  &:hover {
+    cursor: pointer;
+    outline: 1px solid grey;
+    border-radius: 8px;
+  }
+}
+</style>
--- client/components/TasksList.vue
+++ client/components/TasksList.vue
@@ -1,20 +1,32 @@
 <template>
-  <ul aria-label="Pending tasks">
+  <ul aria-label="Pending tasks" class="pending-tasks">
     <li
-      v-for="item in pendingItems"
-      :key="item.id"
+      v-for="task in tasks"
+      :key="task.id"
       class="mt-4 list-item">
-      <TaskItem @update:task="toggleTask" :item="item" />
+      <Transition duration="500" name="nested">
+        <TaskItem
+          v-if="!task.done"
+          @update:task="toggleTask"
+          :item="task"
+          :done="task.done" />
+      </Transition>
     </li>
   </ul>
   <Divider align="center">
     <span id="done-badge" class="p-tag">Done</span>
   </Divider>
-  <ul aria-labelledby="done-badge">
+  <ul aria-labelledby="done-badge" class="done-tasks">
     <li
-      v-for="item in doneItems"
-      :key="item.id">
-      <TaskItem @update:task="toggleTask" :item="item" />
+      v-for="task in tasks"
+      :key="task.id">
+      <Transition duration="500" name="nested">
+        <TaskItem
+          v-if="task.done"
+          @update:task="toggleTask"
+          :item="task"
+          :done="task.done" />
+      </Transition>
     </li>
   </ul>
 </template>
@@ -28,32 +40,39 @@ const props = defineProps({
   items: { type: Array, required: true }
 });
 
-const pendingItems = ref(props.items.filter(it => !it.done));
-const doneItems = ref(props.items.filter(it => it.done));
-
-const moveToDone = task => {
-  removeItemFromList(task, pendingItems);
-  doneItems.value.push(task);
-};
-
-const moveToPending = task => {
-  removeItemFromList(task, doneItems);
-  pendingItems.value.push(task);
-};
+const tasks = ref(props.items);
 
 const toggleTask = ($event, task) => {
   task.done = $event;
-  if ($event) moveToDone(task);
-  else moveToPending(task);
 };
-
-function removeItemFromList(item, list) {
-  list.value = list.value.filter(it => it.id !== item.id);
-}
 </script>
 
 <style lang="scss" scoped>
+@mixin transition($y: 20px) {
+  .nested-enter-active, .nested-leave-active {
+  transition: all 0.3s ease-in-out;
+  }
+  /* delay leave of parent element */
+  .nested-leave-active {
+    transition-delay: 0.25s;
+  }
+
+  .nested-enter-from,
+  .nested-leave-to {
+    transform: translateY($y);
+    opacity: 0;
+  }
+}
+
 .list-item {
   background-color: #f2f2f2;
+}
+
+.pending-tasks {
+  @include transition;
+}
+
+.done-tasks {
+  @include transition(-20px);
 }
 </style>
