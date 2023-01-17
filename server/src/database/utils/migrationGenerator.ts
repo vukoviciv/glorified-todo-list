c357ee51c0400778f975d937543ce326e8800f5e
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-17 15:20:23 +0100

    Cleanup migration generate file

--- server/src/database/utils/migrationGenerator.ts
+++ server/src/database/utils/migrationGenerator.ts
@@ -1,17 +1,16 @@
-import { format } from 'sql-formatter';
 import { TSMigrationGenerator } from '@mikro-orm/migrations';
 
 export class CustomMigrationGenerator extends TSMigrationGenerator {
-  generateMigrationFile(className: string, diff: { up: string[]; down: string[] }): string {
-    const comment = '// this file was generated via custom migration generator\n\n';
-    return comment + super.generateMigrationFile(className, diff);
-  }
+  generateMigrationFile(className: string): string {
+    /* eslint-disable quotes */
+    let ret = `import { Migration } from '@mikro-orm/migrations';\n\n`;
+    ret += `export class ${className} extends Migration {\n`;
+    ret += `  async up(): Promise<void> {\n\n`;
+    ret += `  }\n\n`;
+    ret += `  async down(): Promise<void> {\n\n`;
+    ret += `  }\n`;
+    ret += `}\n`;
 
-  createStatement(sql: string, padLeft: number): string {
-    sql = format(sql, { language: 'postgresql' });
-    // a bit of indenting magic
-    sql = sql.split('\n').map((l, i) => i === 0 ? l : `${' '.repeat(padLeft + 13)}${l}`).join('\n');
-
-    return super.createStatement(sql, padLeft);
+    return ret;
   }
 }
