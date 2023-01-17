aa89b319c40b2483f952091048c88b6ba57bc17e
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-17 14:21:31 +0100

    Migration create process supported

---
+++ server/src/database/utils/migrationFileName.ts
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export function getMigrationFileName() {
+  const timestamp = new Date().toISOString().replace(/[-T:]|\.\d{3}z$/ig, '');
+  const fileName = readlineSync.question('File name: \n>');
+  const parsedFileName = fileName.trim().replace(/\s/g, '-');
+
+  return `${timestamp}-${parsedFileName}`;
+}
