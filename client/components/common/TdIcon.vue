29e645c6c65748cd5931ab389bca9d38e96b5e10
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-18 12:17:42 +0100

    Add TdIcon

---
+++ client/components/common/TdIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <i v-bind="$attrs" aria-hidden="true" :class="icon"></i>
+    <span v-if="text" class="p-sr-only">{{ text }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+};
+</script>
+<script setup>
+defineProps({
+  icon: { type: String, required: true },
+  text: { type: String, default: null }
+});
+</script>
