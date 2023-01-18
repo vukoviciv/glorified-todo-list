090b88991be35b316d42eb9eb0d13ef1127bc37e
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-18 09:48:15 +0100

    add create task migration

--- server/src/database/entities/Task.ts
+++ server/src/database/entities/Task.ts
@@ -1,11 +1,12 @@
-import { Entity, Enum, ManyToOne, Property } from '@mikro-orm/core';
+/* eslint-disable no-unused-vars */
+import { DateType, Entity, Enum, ManyToOne, Property } from '@mikro-orm/core';
 import { Account } from './index';
 import { BaseEntity } from './BaseEntity';
 
 export enum TaskPriority {
-  HIGH = 'high',
-  MEDIUM = 'medium',
-  LOW = 'low'
+  HIGH = 'HIGH',
+  MEDIUM = 'MEDIUM',
+  LOW = 'LOW'
 }
 
 @Entity()
@@ -22,14 +23,19 @@ export class Task extends BaseEntity {
   @Property({ default: false })
     done?: boolean;
 
+  @Property({ type: DateType })
+    deadline: DateType;
+
   @Enum(() => TaskPriority)
     priority!: TaskPriority.MEDIUM; // is this default value for enums?
 
-  constructor(name: string, description: string, account: Account, done: boolean) {
+  constructor(name: string, description: string, account: Account, done: boolean,
+    deadline: DateType) {
     super();
     this.name = name;
     this.description = description;
     this.account = account;
     this.done = done;
+    this.deadline = deadline;
   }
 }
