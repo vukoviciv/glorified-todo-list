d21d5c4a62a74528352377502fdf72656a26b6cc
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-19 13:30:40 +0100

    Add stringProcessors util

--- server/src/routes/middlewares/RequestContext.ts
+++ server/src/routes/middlewares/RequestContext.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { DI } from '../../database/index';
 import { RequestContext } from '@mikro-orm/core';
 
-export function requestContextMiddleware(req: Request, res: Response, next: NextFunction) {
+export function requestContextMiddleware(_req: Request, _res: Response, next: NextFunction) {
   RequestContext.create(DI.em, next);
 }
