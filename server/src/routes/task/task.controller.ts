ba2c30117c58821454f49ccb07f20ed45fd1e576
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-23 14:51:06 +0100

    Fix tasks fetch

--- server/src/routes/task/task.controller.ts
+++ server/src/routes/task/task.controller.ts
@@ -3,10 +3,11 @@ import { Request, Response } from 'express';
 import { DI } from '../../database/index';
 
 async function list(_req: Request, res: Response) {
-  const activeAccount = await DI.em.findOne(Account, 1); // TODO: implement localStorage
-  const tasks = await DI.em.find(Task, {});
-  if (!tasks.length || !activeAccount) return res.send('No items');
+  const accounts = await DI.em.find(Account, {}); // TODO: implement localStorage
+  if (!accounts) return res.send('No accounts');
 
+  const activeAccount = accounts.pop();
+  const tasks = await DI.em.find(Task, { account: activeAccount });
   return res.json(tasks);
 }
 
