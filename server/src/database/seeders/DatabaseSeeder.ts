a03fe600ba83739e9afd9a7c00a636ffba0bb92a
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-23 09:03:22 +0100

    Cleanup seeder

--- server/src/database/seeders/DatabaseSeeder.ts
+++ server/src/database/seeders/DatabaseSeeder.ts
@@ -1,14 +1,28 @@
+import { Account, User } from '../entities';
 import { AccountFactory, TaskFactory, UserFactory } from './Factories';
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 
 export class DatabaseSeeder extends Seeder {
+  #accountCount = 3;
+  #taskCount = 20;
+  #userCount = 2;
+
   async run(em: EntityManager): Promise<void> {
     const users = new UserFactory(em).each(user => {
-      user.accounts.set(new AccountFactory(em).each(account => {
-        account.tasks.set(new TaskFactory(em).make(20));
-      }).make(3));
-    }).make(2);
+      this.setAccountsWithTasks(user, em);
+    }).make(this.#userCount);
+
     em.persist(users);
+  }
+
+  setAccountsWithTasks(user: User, em: EntityManager) {
+    user.accounts.set(new AccountFactory(em).each(account => {
+      this.setTasks(account, em);
+    }).make(this.#accountCount));
+  }
+
+  setTasks(account: Account, em: EntityManager) {
+    account.tasks.set(new TaskFactory(em).make(this.#taskCount));
   }
 }
