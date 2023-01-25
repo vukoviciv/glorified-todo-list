88a46ab6485440c5dd79235617aa9fd96d23e7ee
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-25 13:28:30 +0100

    Support orderBy sort FE to BE

--- server/src/routes/task/task.controller.ts
+++ server/src/routes/task/task.controller.ts
@@ -2,15 +2,14 @@ import { Account, Task } from '../../database/entities';
 import { Request, Response } from 'express';
 import { DI } from '../../database/index';
 
-async function list(req: Request, res: Response) {
+async function list({ query }: Request, res: Response) {
   const accounts = await DI.em.find(Account, {}); // TODO: implement localStorage
   if (!accounts) return res.send('No accounts');
 
-  const { query } = req;
-  const defaultOrderBy = { priority: 'ASC', name: 'ASC' } as const;
+  const options = { orderBy: {} };
+  if (query.orderBy) options.orderBy = query.orderBy;
   const activeAccount = accounts.pop();
-  const orderBy = Object.assign({}, defaultOrderBy, query.orderBy);
-  const tasks = await DI.em.find(Task, { account: activeAccount }, { orderBy });
+  const tasks = await DI.em.find(Task, { account: activeAccount }, options);
 
   return res.json(tasks);
 }
