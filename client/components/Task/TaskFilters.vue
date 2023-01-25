d1882420b46ecf7589b6f7399ad293ea38719913
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-25 12:49:27 +0100

    Add description and createdAt checkboxes

--- client/components/Task/TaskFilters.vue
+++ client/components/Task/TaskFilters.vue
@@ -1,27 +1,43 @@
 <template>
-  <div class="task-filters mt-2 mb-4 flex">
-    <div
-      v-for="filter in filters"
-      :key="filter"
-      class="field-checkbox ml-8">
+  <div class="task-filters mt-2 mb-4 flex flex-wrap">
+    <Dropdown
+      v-model="selectedOrderBy"
+      :options="orderByValues"
+      input-id="test-input-id"
+      option-label="name"
+      option-value="value"
+      placeholder="Order by" />
+    <div class="field-checkbox ml-8">
       <Checkbox
-        v-model="checked"
-        @change="$emit('filters:update', checked)"
-        :value="filter"
-        :input-id="`order-by-${filter}`"
-        :name="`order-by-${filter}`" />
-      <label for="`order-by-${filter}`">Order by: <b>{{ filter }}</b></label>
+        v-model="showDescription"
+        @input="$emit('options:update', { showDescription: $event})"
+        input-id="show-description"
+        :binary="true" />
+      <label for="show-description">Show <b>description</b></label>
+    </div>
+    <div class="field-checkbox ml-8">
+      <Checkbox
+        v-model="showCreatedAt"
+        @input="$emit('options:update', { showCreatedAt: $event })"
+        input-id="show-created-at"
+        :binary="true" />
+      <label for="show-created-at">Show <b>created at</b></label>
     </div>
   </div>
 </template>
 
 <script setup>
 import Checkbox from 'primevue/checkbox';
+import Dropdown from 'primevue/dropdown';
 import { ref } from 'vue';
 
-defineProps({
-  filters: { type: Array, required: true }
+const props = defineProps({
+  orderByValues: { type: Array, required: true },
+  showDescription: { type: Boolean, required: true },
+  showCreatedAt: { type: Boolean, require: true }
 });
 
-const checked = ref([]);
+const selectedOrderBy = ref();
+const showDescription = ref(props.showDescription);
+const showCreatedAt = ref(props.showCreatedAt);
 </script>
--- client/components/Task/TaskList.vue
+++ client/components/Task/TaskList.vue
@@ -3,13 +3,15 @@
     <li
       v-for="item in items"
       :key="item.id"
-      class="mt-3 list-item">
+      class="mt-4 list-item">
       <Transition duration="500" name="nested">
         <TaskItem
           v-if="doneTasks ? item.done : !item.done"
           @update:task="$emit('update:task', $event)"
           :item="item"
-          :done="item.done" />
+          :done="item.done"
+          :show-description="showDescription"
+          :show-created-at="showCreatedAt" />
       </Transition>
     </li>
   </ul>
@@ -20,7 +22,9 @@ import TaskItem from './TaskItem.vue';
 
 const props = defineProps({
   items: { type: Array, required: true },
-  doneTasks: { type: Boolean, default: false }
+  doneTasks: { type: Boolean, default: false },
+  showDescription: { type: Boolean, required: true },
+  showCreatedAt: { type: Boolean, require: true }
 });
 
 const className = props.doneTasks ? 'done-tasks' : 'pending-tasks';
