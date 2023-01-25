d0b0af45114ac1d2536b645a54fba7d1a2a83a28
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-25 15:56:45 +0100

    Add task through the modal

--- client/components/CreateTaskDialog.vue
+++ client/components/CreateTaskDialog.vue
@@ -11,11 +11,39 @@
     :breakpoints="{ '960px': '75vw', '640px': '90vw' }"
     :style="{ width: '50vw' }"
     :modal="true"
-    header="Header"
+    header="Create new task"
     position="top">
-    <p class="m-0">
-      {{ task }}
-    </p>
+    <div class="m-0 p-3">
+      <div class="p-float-label flex">
+        <InputText
+          v-model="task.name"
+          id="name"
+          type="text"
+          required="required"
+          class="flex flex-grow-1" />
+        <label for="name">Name</label>
+      </div>
+      <div class="p-float-label mt-5 flex">
+        <Textarea
+          v-model="task.description"
+          id="description"
+          :auto-resize="true"
+          rows="5"
+          required="required"
+          class="flex flex-grow-1" />
+        <label for="description">Description</label>
+      </div>
+      <div class="mt-5 flex">
+        <Dropdown
+          v-model="task.priority"
+          :options="priorities"
+          input-id="priority"
+          option-label="name"
+          option-value="value"
+          class="flex flex-grow-1"
+          placeholder="Priority" />
+      </div>
+    </div>
     <template #footer>
       <Button
         @click="close()"
@@ -25,31 +53,44 @@
       <Button
         @click="create()"
         label="Create task"
-        icon="pi pi-check"
+        :icon="createBtnIcon"
         autofocus />
     </template>
   </Dialog>
 </template>
 
 <script setup>
+// TODO: create app lvl notification aria-live="polite" container
+// TODO: check label
 import Button from 'primevue/Button';
 import Dialog from 'primevue/Dialog';
+import Dropdown from 'primevue/dropdown';
+import InputText from 'primevue/inputtext';
 import { ref } from 'vue';
 import taskApi from '@/src/api/tasks';
+import Textarea from 'primevue/textarea';
 
+const priorities = [
+  { name: 'High', value: 'HIGH' },
+  { name: 'Medium', value: 'MEDIUM' },
+  { name: 'Low', value: 'LOW' }
+];
+const DEFAULT_CREATE_BTN_ICON = 'pi pi-check';
+const createBtnIcon = ref(DEFAULT_CREATE_BTN_ICON);
 const displayPosition = ref(false);
-const task = {
-  name: 'Test task from the modal',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
-  done: false,
-  priority: 'HIGH'
-};
+const task = ref({
+  name: '',
+  description: '',
+  priority: ''
+});
 
 const open = () => { displayPosition.value = true; };
 const close = () => { displayPosition.value = false; };
 const create = () => {
-  taskApi.create(task).then(data => {
-    console.log({ data });
+  createBtnIcon.value = 'pi pi-spin pi-spinner';
+  taskApi.create(task.value).then(data => {
+    createBtnIcon.value = DEFAULT_CREATE_BTN_ICON;
+    close();
   });
 };
 </script>
