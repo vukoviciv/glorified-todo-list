1f41b69ed9dcd2b287128df4c2927a39604af2cb
isimic@extensionengine.com <isimic@extensionengine.com>
2023-01-31 15:08:35 +0100

    Tasks seeder

---
+++ server/src/database/seeders/TaskSeeder.ts
@@ -0,0 +1,24 @@
+import { Account, User } from '../entities';
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import { TaskFactory } from './factories';
+
+export class TaskSeeder extends Seeder {
+  #taskCount = 20;
+
+  async run(em: EntityManager): Promise<void> {
+    const users = await em.find(User, {});
+    const user = users.pop();
+    if (!user) throw new Error('No users in the database!');
+    console.log(`For user: ${user.firstName} ${user.lastName} (ID: ${user.id})`);
+
+    const accounts = await em.find(Account, { user });
+    const account = accounts.pop();
+    if (!account) throw new Error('No accounts in the database!');
+    console.log(`For account: ${account.name} (ID: ${account.id})`);
+
+    account.tasks.set(new TaskFactory(em).make(this.#taskCount));
+    console.log(`${this.#taskCount} tasks successfully created!`);
+    em.persistAndFlush(account);
+  }
+}
