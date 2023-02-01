e5321fbfb938e13768aef9761dae108deca533d7
isimic@extensionengine.com <isimic@extensionengine.com>
2023-02-01 14:38:08 +0100

    Introduce background

--- client/auth/src/components/AppLogin.vue
+++ client/auth/src/components/AppLogin.vue
@@ -1,28 +1,37 @@
 <template>
-  <form>
-    <Card class="m-auto mt-5 md:col-6">
-      <template #title>
-        <h2 class="text-center">Login</h2>
-      </template>
-      <template #content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-        consequuntur error repudiandae numquam deserunt quisquam repellat libero
-        asperiores earum nam nobis, culpa ratione quam perferendis esse,
-        cupiditate neque quas!
-      </template>
-      <template #footer>
-        <Button icon="pi pi-check" label="Save" type="submit" />
-        <Button
-          icon="pi pi-times"
-          label="Cancel"
-          class="p-button-secondary"
-          style="margin-left: 0.5em" />
-      </template>
-    </Card>
-  </form>
+  <div class="login-wrapper min-h-screen">
+    <form class="pt-5">
+      <Card class="m-auto md:col-6">
+        <template #title>
+          <h2 class="text-center">Login</h2>
+        </template>
+        <template #content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+          consequuntur error repudiandae numquam deserunt quisquam repellat libero
+          asperiores earum nam nobis, culpa ratione quam perferendis esse,
+          cupiditate neque quas!
+        </template>
+        <template #footer>
+          <Button icon="pi pi-check" label="Save" type="submit" />
+          <Button
+            icon="pi pi-times"
+            label="Cancel"
+            class="p-button-secondary"
+            style="margin-left: 0.5em" />
+        </template>
+      </Card>
+    </form>
+  </div>
 </template>
 
 <script setup>
 import Button from 'primevue/button';
 import Card from 'primevue/card';
 </script>
+
+<style lang="scss" scoped>
+.login-wrapper {
+  background: url(../assets/auth_background.jpg);
+  background-size: cover;
+}
+</style>
