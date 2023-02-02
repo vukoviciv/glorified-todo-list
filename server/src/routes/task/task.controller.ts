9a70c42232f071c41dd319810929891e6a266036
isimic@extensionengine.com <isimic@extensionengine.com>
2023-02-02 14:48:29 +0100

    Add lodash. Fix updateTask to bump updatedAt field

--- server/src/routes/task/task.controller.ts
+++ server/src/routes/task/task.controller.ts
@@ -2,11 +2,9 @@ import { Request, Response } from 'express';
 import { DI } from '../../database/index';
 import { Task } from '../../database/entities';
 
-async function list({ query, body }: Request, res: Response) {
-  const { account } = body;
+async function list({ query, body: { account } }: Request, res: Response) {
   const options = { orderBy: {} };
   if (query.orderBy) options.orderBy = query.orderBy;
-
   const tasks = await DI.em.find(Task, { account }, options);
 
   return res.json(tasks);
@@ -20,17 +18,26 @@ async function create({ body }: Request, res: Response) {
 }
 
 async function update({ body, params: { id } }: Request, res: Response) {
-  const task = await DI.em.upsert(Task, { id, ...body });
+  const fields = { fields: ['name', 'description', 'deadline', 'priority'] } as const;
+  const task = await DI.em.findOne(Task, { id: parseInt(id) }, fields);
+  if (!task) throw new Error(`Task with ID: ${id} does not exist!`);
+
+  task.name = body?.name;
+  task.description = body?.description;
+  task.deadline = body?.deadline;
+  task.priority = body?.priority;
+  await DI.em.persistAndFlush(task);
 
   return res.json(task);
 }
 
 async function toggleDone({ params: { id } }: Request, res: Response) {
   const task = await DI.em.findOne(Task, { id: parseInt(id) });
-  if (!task) throw new Error('No task found!');
+  if (!task) throw new Error(`Task with ID: ${id} does not exist!`);
 
   task.done = !task.done;
   await DI.em.persistAndFlush(task);
+
   return res.json(task);
 }
 
