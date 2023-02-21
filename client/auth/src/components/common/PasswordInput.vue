843a52381a4f5905c892f35bfa91de45da8aaf35
isimic@extensionengine.com <isimic@extensionengine.com>
2023-02-21 19:39:42 +0100

    Add PasswordInput common component

---
+++ client/auth/src/components/common/PasswordInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="p-inputgroup">
+    <div class="p-float-label">
+      <InputText
+        v-model="password"
+        @update:model-value="$emit('updated', { password })"
+        id="password"
+        :type="passwordFieldType"
+        required="required"
+        class="flex flex-grow-1" />
+      <label for="password">Password</label>
+    </div>
+    <Button
+      @click="handlePassIconClick()"
+      :icon="passwordIcon"
+      :aria-label="passwordLabel"
+      :title="passwordLabel" />
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import Button from 'primevue/Button';
+import InputText from 'primevue/inputtext';
+import { PrimeIcons } from 'primevue/api';
+
+const PASS_TYPES = {
+  PASSWORD: {
+    value: 'password',
+    label: 'Show password'
+  },
+  TEXT: {
+    value: 'text',
+    label: 'Hide password'
+  }
+};
+const password = ref('');
+const passwordFieldType = ref('password');
+
+const isPassHidden = computed(() => {
+  return passwordFieldType.value === PASS_TYPES.PASSWORD.value;
+});
+const passwordIcon = computed(() => {
+  return isPassHidden.value ? PrimeIcons.EYE : PrimeIcons.EYE_SLASH;
+});
+const passwordLabel = computed(() => {
+  return isPassHidden.value ? PASS_TYPES.PASSWORD.label : PASS_TYPES.TEXT.label;
+});
+const handlePassIconClick = () => {
+  passwordFieldType.value = isPassHidden.value ? PASS_TYPES.TEXT.value : PASS_TYPES.PASSWORD.value;
+};
+</script>
