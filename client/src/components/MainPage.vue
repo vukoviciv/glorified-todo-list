e4d4f3fb6ccd5affd179c35603b973eba9f4db7e
isimic@extensionengine.com <isimic@extensionengine.com>
2023-03-01 12:34:09 +0100

    Remove unused component

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -2,23 +2,14 @@
   <main class="main-container p-5 m-auto">
     <TasksMain
       @update:order="updateOrder" />
-    <AccountsDialog
-      v-if="showDialog"
-      @account:switch="$emit('account:switch', $event)" />
   </main>
 </template>
 
 <script setup>
-import AccountsDialog from './AccountsDialog.vue';
-import { computed } from 'vue';
 import { orderBy } from '@/config/task';
 import TasksMain from './Task/index.vue';
-import { useStore } from 'vuex';
 
-const store = useStore();
 const emit = defineEmits(['account:switch', 'fetch']);
-
-const showDialog = computed(() => store.getters?.getActiveAccounts);
 
 const updateOrder = ({ value }) => {
   const item = orderBy.list[value];
