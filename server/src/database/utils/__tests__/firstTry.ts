b0253dfd036ce336b616dcd67c66e1a9e388f7b4
isimic@extensionengine.com <isimic@extensionengine.com>
2023-03-13 12:13:39 +0100

    Add jest-cases

--- server/src/database/utils/__tests__/firstTry.ts
+++ server/src/database/utils/__tests__/firstTry.ts
@@ -1,4 +1,5 @@
 import { isPasswordAllowed, sum } from '../firstTry';
+import cases from 'jest-in-case';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,14 +7,49 @@ test('adds 1 + 2 to equal 3', () => {
 
 // password
 
-test('isPasswordAllowed returns true for valid password', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true);
-});
+cases(
+  'isPasswordAllowed: valid passwords',
+  options => {
+    expect(isPasswordAllowed(options.password)).toBe(true);
+  }, {
+    'valid password': {
+      password: '!aBc123'
+    }
+  }
+);
 
-test('isPasswordAllowed returns false for invalid password', () => {
-  expect(isPasswordAllowed('a2c!')).toBe(false);
-  expect(isPasswordAllowed('123456!')).toBe(false);
-  expect(isPasswordAllowed('ABC123!')).toBe(false);
-  expect(isPasswordAllowed('abc123!')).toBe(false);
-  expect(isPasswordAllowed('ABCdef123')).toBe(false);
+cases(
+  'isPasswordAllowed: invalid passwords',
+  options => {
+    expect(isPasswordAllowed(options.password)).toBe(false);
+  }, {
+    'too short': { password: 'a2c!' },
+    'no letters': { password: '123456!' },
+    'no numbers': { password: 'ABCdef!' },
+    'no lowercase': { password: 'ABC123!' },
+    'no uppercase': { password: 'abc123!' },
+    'no non-alphanumerical char': { password: 'ABCdef123' }
+  }
+);
+
+describe('isPasswordAllowed only allows some passwords', () => {
+  const allowedPasswords = ['!aBc123'];
+  const disallowedPasswords = [
+    'a2c!',
+    '123456!',
+    'ABCdef!',
+    'ABC123!',
+    'abc123!',
+    'ABCdef123'
+  ];
+  allowedPasswords.forEach(password => {
+    test(`allows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(true);
+    });
+  });
+  disallowedPasswords.forEach(password => {
+    test(`disallows ${password}`, () => {
+      expect(isPasswordAllowed(password)).toBe(false);
+    });
+  });
 });
