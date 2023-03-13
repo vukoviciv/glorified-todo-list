d4658eb0bacb31ef77b4a6f37a71c36ce8b2716f
isimic@extensionengine.com <isimic@extensionengine.com>
2023-03-13 07:39:09 +0100

    Add watch script and new method for testing

--- server/src/database/utils/__tests__/firstTry.ts
+++ server/src/database/utils/__tests__/firstTry.ts
@@ -1,5 +1,19 @@
-import { sum } from '../firstTry';
+import { isPasswordAllowed, sum } from '../firstTry';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+// password
+
+test('isPasswordAllowed returns true for valid password', () => {
+  expect(isPasswordAllowed('!aBc123')).toBe(true);
+});
+
+test('isPasswordAllowed returns false for invalid password', () => {
+  expect(isPasswordAllowed('a2c!')).toBe(false);
+  expect(isPasswordAllowed('123456!')).toBe(false);
+  expect(isPasswordAllowed('ABC123!')).toBe(false);
+  expect(isPasswordAllowed('abc123!')).toBe(false);
+  expect(isPasswordAllowed('ABCdef123')).toBe(false);
 });
--- server/src/database/utils/firstTry.ts
+++ server/src/database/utils/firstTry.ts
@@ -1,3 +1,17 @@
 export function sum(a: number, b: number) {
   return a + b;
 }
+
+export function isPasswordAllowed(password: string) {
+  return (
+    password.length > 6 &&
+    // non-alphanumeric
+    /\W/.test(password) &&
+    // digit
+    /\d/.test(password) &&
+    // capital letter
+    /[A-Z]/.test(password) &&
+    // lowercase letter
+    /[a-z]/.test(password)
+  );
+}
